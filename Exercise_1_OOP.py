b46da4f718008eb378751777beeace53fd3cfad9
Klaudia Ramiaczek <klaudia.ramiaczek@edu.uekat.pl>
2023-12-18 15:57:10 +0100

    Dodaj zadania

---
+++ Exercise_1_OOP.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def is_passed(self):
+        average_marks = sum(self.marks) / len(self.marks)
+        return average_marks > 50
+
+
+student1 = Student("Katarzyna Kowalska", [60, 70, 80])
+student2 = Student("Ignacy Ćwiąkała", [40, 30, 20])
+
+print(student1.is_passed())
+print(student2.is_passed())
